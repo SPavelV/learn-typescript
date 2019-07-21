126392003657287c14be8c24ec0d504222adc6cd
PavelSolovev <pashius2010@gmail.com>
2019-07-22 01:12:41 +0400

    variables

---
+++ 03.samples/01-variables/01-variables.ts
@@ -0,0 +1,10 @@
+//Определение переменной с именем messag1 и значением Hello без явного указания тип
+let message = "Hello"!;
+
+// Определение переменной с именем messag2 типа string и значением Hello! без явного указания тип
+let message2: string = "Hello!";
+
+// Тоже что и let, но созданное значение нельзя изменить.
+const message3: string = "Hello!"
+
+// message3 = "wordl" //[ts] Cannot assign to 'message3' because it is a constant or read-only property 
